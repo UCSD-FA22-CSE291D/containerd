01bde32c38b5aac8d3e0fd1f86dbf20bc267e7be
Qihang <qizeng@ucsd.edu>
2022-11-23 21:12:39 -0800

    move predump and parentpath to opts

--- runtime/v2/runc/container.go
+++ runtime/v2/runc/container.go
@@ -478,9 +478,9 @@ func (c *Container) Checkpoint(ctx context.Context, r *task.CheckpointTaskReques
 	}
 	return p.(*process.Init).Checkpoint(ctx, &process.CheckpointConfig{
 		Path:                     r.Path,
-		ParentPath:               r.ParentPath,
+		ParentPath:               opts.ParentPath,
 		Exit:                     opts.Exit,
-		Predump:                  r.Predump,
+		Predump:                  opts.Predump,
 		AllowOpenTCP:             opts.OpenTcp,
 		AllowExternalUnixSockets: opts.ExternalUnixSockets,
 		AllowTerminal:            opts.Terminal,
