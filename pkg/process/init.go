d0d1ed06dbebd6b9722efaa914225e2bbaa10583
Qihang <qizeng@ucsd.edu>
2022-11-29 16:30:16 -0800

    done

--- pkg/process/init.go
+++ pkg/process/init.go
@@ -428,6 +428,7 @@ func (p *Init) checkpoint(ctx context.Context, r *CheckpointConfig) error {
 	if !r.Exit {
 		actions = append(actions, runc.LeaveRunning)
 	}
+	actions = append(actions, runc.PreDump)
 	// keep criu work directory if criu work dir is set
 	work := r.WorkDir
 	if work == "" {
